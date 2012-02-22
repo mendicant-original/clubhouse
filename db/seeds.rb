f1d357c1e48227c41d20ec0da85684d7724d3991
Jordan Byron <jordan.byron@gmail.com>
2012-02-22 15:45:12 -0500

    Add seed data for roles, groups, and resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Resources
+
+Resource.create(name: "Clubhouse")
+Resource.create(name: "Community")
+Resource.create(name: "Liskov")
+
+# Groups
+
+Group.create(name: "Visitor")
+Group.create(name: "Applicant")
+Group.create(name: "Alumnus")
+Group.create(name: "Guest")
+Group.create(name: "Staff")
+
+# Roles
+
+Role.create(name: "Member")
+Role.create(name: "Manager")
+Role.create(name: "Administrator")
