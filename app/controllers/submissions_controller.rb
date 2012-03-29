516d873af5a4ad8a04b63968dd0d5c1d9434d185
Nerian <siotopo@gmail.com>
2012-03-30 00:17:18 +0200

    Create the user when he submits the submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,14 +2,24 @@ class SubmissionsController < ApplicationController
   def new
     @admission = Admission.find(params[:admission_id])
     @submission = Submission.new
-    @admission.admission_questions.each do |admission_question|  
+    @admission.admission_questions.each do |admission_question|
       @submission.question_responses.new(:admission_question_id => admission_question.id)
-    end    
+    end
   end
 
   def create
     @admission = Admission.find(params[:admission_id])
-    @admission.submissions.new(params[:submission])    
+    @submission = @admission.submissions.new(params[:submission])    
+    
+    @person = Person.new(:authorization_id => '2343',
+      :group_id => '123123',
+      :email => params[:person][:email],
+      :website => params[:person][:website],
+      :name => params[:person][:name])
+    @person.save
+      
+    @submission.person = @person
+
     @admission.save
   end
 end
